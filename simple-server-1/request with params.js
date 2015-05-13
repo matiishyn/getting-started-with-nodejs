1c99b545b5528ee4b06c09da5473ffbe55ec2db7
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2015-05-13 16:43:44 +0200

    simple servers with request params

--- simple-server-1/request with params.js
+++ simple-server-1/request with params.js
@@ -1,9 +1,20 @@
-var http = require('http');
+var http = require('http'),
+    url = require('url');
 
 var server = new http.Server(); // http.Server -> net.Server -> EventEmitter
 
 server.listen(8080, 'localhost');
 
-server.on('request', function(req, res) {
-    res.end("Hello");
+server.on('request', function (req, res) {
+    var urlParsed = url.parse(req.url, true);
+
+    if (req.method === 'GET' && urlParsed.pathname === '/echo' && urlParsed.query.message) {
+        res.end(urlParsed.query.message);
+        return;
+    }
+
+    res.statusCode = 404;
+    res.end("Not Found");
 });
+
+// http://localhost:8080/echo?message=text -> text
