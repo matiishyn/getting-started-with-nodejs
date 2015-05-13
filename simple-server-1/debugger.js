fe2bbdef90e39542252ddffd738b440eddd214a6
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2015-05-13 16:50:41 +0200

    simple servers with request params

---
+++ simple-server-1/debugger.js
@@ -0,0 +1,21 @@
+var http = require('http'),
+    url = require('url');
+
+var server = new http.Server(); // http.Server -> net.Server -> EventEmitter
+
+server.listen(8080, 'localhost');
+
+server.on('request', function (req, res) {
+    var urlParsed = url.parse(req.url, true);
+
+    debugger;
+
+    if (req.method === 'GET' && urlParsed.pathname === '/echo' && urlParsed.query.message) {
+        res.end(urlParsed.query.message);
+        return;
+    }
+
+    res.statusCode = 404;
+    res.end("Not Found");
+});
+// > node debug app.js
